6b8b8b8f52a5c9fb2586bb70f79b8fa75ef5de20
Linus Hof <linus.hof@tum.de>
2025-10-28 13:55:40 +0100

    code session 2 and update session 1

--- code/session_01_hello.R
+++ code/session_01_hello.R
@@ -18,35 +18,10 @@ o2 - 2 == o1
 
 # Exercise 3
 
+v1 <- c(1:5, 'b')
+
+
 x <- 1:5
 (((x + 2) * 3) - 6) / 3
-
-# Exercise 4 
-
-n_obs <- 5
-dat <- data.frame(v1 = rnorm(n_obs, mean=1),
-                  v2 = rnorm(n_obs, mean=2),
-                  v3 = rnorm(n_obs, mean=3),
-                  v4 = rnorm(n_obs, mean=4),
-                  v5 = rnorm(n_obs, mean=5))
-
-c(mean(dat[,1]), mean(dat[,2]), mean(dat[,3]), mean(dat[,4]), mean(dat[,5]))
-sapply(dat, mean)
-?sapply()
-colMeans(dat)
-
-
-# Exercise 5
-
-sequence <- 1:5
-output <- vector('numeric', length(sequence))
-
-for (i in sequence){ 
-  
-  output[i] <- mean(dat[,i])
-  
-  }
-
-output
 
 
